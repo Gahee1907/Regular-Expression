6638bf9fd63972dcf5795fb1afb6314faa2ecec1
Gahee1907 <Gahee1907@naver.com>
2024-12-15 15:40:43 +0900

    meta character

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 #Regular-Expression
+
+* 옵션 (g, i, m)
+global: 첫번째 감지에 의한 결과 뿐 아니라, 전체 영역에서 전부를 탐색.
+ignore: 대소문자의 구분을 무시하는 옵션
+multi-line: "\n"기준으로 하나의 줄로 인식. 각각 하나의 줄이 하나의 물자열로 처리. ex) the lazy dog. 는 마지막 문자가 "."임.
+
+* regular expression
+1) /./ 
+".": 임의의 하나의 문자를 의미.
+2) /\.$/ 
+"\": 다음의 문자를 단순히 문자로 처리.
+"$": 하나의 줄의 끝 문자를 확인.
+
+* Pattern(string)
+1) ^(string)
+"^"caret: 한줄 문자열에서 제일 앞쪽에서 탐색
+2) (string)$
+"$"dollar sign: 한줄 문자열에서 제일 뒤쪽에서 탐색
+3) (string)|(string)
+"|"vertical bar: or(또는)의 의미를 가짐. 여러 문자열을 동시에 탐색
+
+* Pattern(character)
+1) .(dot): 패턴 속에서의 dot는 임의의 한 문자를 의미. 어떤 문자여도 상관없음.
+2) (abc)d?
+"?"question mark: 바로 앞의 문자만이 대상. 바로 앞 문자가 있거나 없거나 모두 해당.
+3) (abc)d*
+"*"asterisk: 바로 앞의 문자만이 대상. 바로 앞 문자가 0번 이상 반복되면 모두 해당.
+4) (abc)d+
+"+"plus sign: 바로 앞의 문자만이 대상.바로 앞 문자가 1번 이상 반복되면 모두 해당. 
+
+* Character set
+: 문자 세트를 구성해 두고 세트에 속하는 문자들은 모두 선택.
+1) []:대괄호를 사용해 세트를 구성. 세트 속에서 구성된 문자 중에서 일치되는 문자 탐색. 
+2) ^: 구성된 문제세트만 배제시킴.
+
+* Meta character
+1) d(decimal)
+\d : [0-9]
+\D : [^0-9]
+2) w(word)
+\w : [a-zA-Z0-9_]
+\W : [^a-zA-Z0-9_]
+3) s(space)
+\s: [ \t\n\r]
+\S: [^ \t\n\r]
+
--- main.js
+++ main.js
@@ -3,16 +3,53 @@ const str = `
 thesecon@gmail.com
 https://www.gildong.com/?p1=1412$p2=alskqwer
 The quick brown fox jumps over the lazy dog.
-abbcccdddd
+abbcccdddd, hippy.
+http://www.naver.com
 `;
 
 // const regexp = new RegExp('the', 'g');
 // const regexp = /the/gi;  regular expression
 
-// let regexp = /./gi;
+/*
+let regexp = /./gi;
+regexp = /\.$/gi;  
+regexp = /\.$/gim;  
 
-let regexp = /\.$/gi;
-console.log('(1)',str.match(regexp));
+//Pattern (string)
+console.log('(1)', str.match(/^the/g)); 
+console.log('(2)', str.match(/^the/gm));
+console.log('(3)', str.match(/^the/gmi));
+console.log('(4)', str.match(/com$/gm));
+console.log('(5)', str.match(/fox|dog/g));
 
-regexp = /\.$/gim;
-console.log('(2)', str.match(regexp));
+
+
+//Pattern (character)
+console.log('(1)', str.match(/h..p/g));
+console.log('(2)', str.match(/https?/g));
+console.log('(3)', str.match(/dd/g));
+console.log('(4)', str.match(/b+/g));
+
+
+//character set
+console.log('(1)', str.match(/[]/g)); //비어있음
+console.log('(2)', str.match(/[abc]/g)); //a,b,c중 일치 된 문자 모두 출력
+console.log('(3)', str.match(/[0-9A-Z]/g)); //0~9, A~Z(대문자)
+
+console.log('(4)', str.match(/[a-z]/g));
+console.log('(5)', str.match(/[^a-z]/g));
+
+// Meta character
+console.log('(1)', str.match(/\d/g));
+console.log('(2)', str.match(/[0-9]/g));
+
+console.log('(3)', str.match(/\w/g));
+console.log('(4)', str.match(/[a-zA-Z0-9_]/g)); //a~z, A~Z, 0~9, _
+
+console.log('(5)', str.match(/\s/g));
+console.log('(6)', str.match(/[ \t\n\r]/g)); // 공백, \tab, \n,\r 
+*/
+
+console.log('(1)', str.match(/b{2}/g));
+console.log('(2)', str.match(/c{1,3}/g));
+console.log('(3)', str.match(/d{1,}/g));
